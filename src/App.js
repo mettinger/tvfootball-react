714a5a97efb7424393a55f837a52ce5e41068b80
mettinger <jettinger35@gmail.com>
2019-02-13 15:00:21 -0500

    message

--- src/App.js
+++ src/App.js
@@ -1,32 +1,78 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 import web3 from './ethereum/web3';
-import SlotMachine from './ethereum/simpleStorage';
-import { Button } from 'semantic-ui-react';
+import SimpleStorage from './ethereum/simpleStorage';
+import { Button, Container, Grid } from 'semantic-ui-react';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { value: ''};
+    SimpleStorage.events.Change({}, (error, events) => { this.setState({value: events.returnValues.newVal })});
+  }
+
+  setValue = async newValue => {
+    //event.preventDefault();
+    const accounts = await web3.eth.getAccounts();
+
+    await SimpleStorage.methods.set(newValue).send({
+      from: accounts[0],
+    });
+
+    this.setState({value: newValue });
+  };
+
+  clickOne = () => {
+    console.log('one');
+    this.setValue(1);
+  }
+
+  clickTwo = () => {
+    console.log('two');
+    this.setValue(2);
+  }
+
+  clickThree = () => {
+    console.log('three');
+    this.setValue(3);
+  }
+
+  clickFour = () => {
+    console.log('four');
+    this.setValue(4);
+  }
+
+  async componentDidMount() {
+    const value = await SimpleStorage.methods.get().call();
+    this.setState({ value });
+
+    /*
+    SimpleStorage.getPastEvents("allEvents",
+                                {fromBlock: 0, toBlock: 'latest'},
+                                (error, events) => { console.log(events); });*/
+  }
+
   render() {
     return (
-      <div className="App">
-      <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.11/semantic.min.css"></link>
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          <Button primary>Enter</Button>
-        </header>
-      </div>
+      <Container style={{ marginTop: '3em' }}>
+        <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.11/semantic.min.css"></link>
+
+        <Grid>
+          <Grid.Row>
+            <Button primary onClick={this.clickOne}>1</Button>
+            <Button primary onClick={this.clickTwo}>2</Button>
+          </Grid.Row>
+
+          <Grid.Row>
+            <Button primary onClick={this.clickThree}>3</Button>
+            <Button primary onClick={this.clickFour}>4</Button>
+          </Grid.Row>
+        </Grid>
+        <div>{this.state.value}</div>
+      </Container>
+
     );
   }
 }
